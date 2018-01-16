367e18d17c28f35c40b6368507b67a9c6f4cdc8a
afageary <abufageary@gmail.com>
2018-01-16 11:02:16 +0000

    add passed wih most recent on top of list

--- src/main/java/com/develogical/RecentlyUsedList.java
+++ src/main/java/com/develogical/RecentlyUsedList.java
@@ -1,7 +1,29 @@
 package com.develogical;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class RecentlyUsedList {
+
+    private ArrayList list;
+
+    public RecentlyUsedList(){
+        list = new ArrayList();
+    }
+
     public boolean isEmpty() {
-        return true;
+        return list.isEmpty();
+    }
+
+    public void add(int i) {
+        ArrayList copy = new ArrayList();
+        copy.add(i);
+        copy.addAll(list);
+        list = copy;
+    }
+
+    public <T> T get(int i) {
+        return (T)list.get(i);
     }
 }
--- src/test/java/com/develogical/RecentlyUsedListTest.java
+++ src/test/java/com/develogical/RecentlyUsedListTest.java
@@ -2,7 +2,10 @@ package com.develogical;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class RecentlyUsedListTest {
 
@@ -12,4 +15,29 @@ public class RecentlyUsedListTest {
         assertTrue(list.isEmpty());
 
     }
+    @Test
+    public void addToList() {
+        RecentlyUsedList list = new RecentlyUsedList();
+        list.add(123);
+        assertFalse(list.isEmpty());
+
+    }
+
+    @Test
+    public void getFromList(){
+        RecentlyUsedList list = new RecentlyUsedList();
+        list.add(1);
+        assertThat(1,equalTo(list.get(0)));
+
+    }
+
+    @Test
+    public void mostRecentItemonTop() {
+        RecentlyUsedList list = new RecentlyUsedList();
+        list.add(5);
+        list.add(7);
+        list.add(10);
+        assertThat(10, equalTo(list.get(0)));
+    }
+
 }
